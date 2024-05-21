68d606573695b9d989bfe3381875b7b28af601aa
hacdan <david.mcclellan.p@gmail.com>
2024-05-21 16:09:35 -0400

    Working on 2-3 routing

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -10,10 +15,41 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{}
+
+	mux.HandleFunc("GET /healthz", HandleHealthz)
+	mux.HandleFunc("GET /metrics", apiCfg.HandleMetrics)
+	mux.HandleFunc("/reset", apiCfg.HandleReset)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+}
 
+func HandleHealthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) HandleMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hitsResponse := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Write([]byte(hitsResponse))
+}
+
+func (cfg *apiConfig) HandleReset(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
 }
